8268265cd249f7969cf30798a28e920c5c9fb1cf
Dang Cong Duc <dangcongduc38@gmail.com>
2025-05-13 09:33:40 +0700

    chore: use custom class custom-component

--- ShopeeClone/src/Components/Footer/Footer.tsx
+++ ShopeeClone/src/Components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='py-16 bg-neutral-100'>
-      <div className='max-w-7xl mx-auto px-4'>
+      <div className='custom-container'>
         <div className='grid gird-cols-1 lg:grid-cols-3 gap-4'>
           <div className='lg:col-span-1'>
             <div>© 2025 Shopee. Tất cả các quyền được bảo lưu.</div>

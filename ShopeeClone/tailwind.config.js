857caafbbfd665c82307e5c323a78ee51b6ce530
Dang Cong Duc <dangcongduc38@gmail.com>
2025-05-13 09:32:24 +0700

    chore: add file .eslintignore and custom class custom-component

--- ShopeeClone/tailwind.config.js
+++ ShopeeClone/tailwind.config.js
@@ -1,6 +1,13 @@
+import { kMaxLength } from 'buffer'
+
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
   theme: {
     extend: {
       colors: {
@@ -9,6 +16,19 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        // Custom class: custom-container
+        '.custom-container': {
+          maxWidth: '80rem',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: '1rem',
+          paddingRight: '1rem',
+        }
+      })
+    })
+  ],
 }
 

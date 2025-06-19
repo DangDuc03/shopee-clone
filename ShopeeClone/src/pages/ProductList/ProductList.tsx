c82fad6b9b86d69911f23cc02e09bc098d6b41fa
Dang Cong Duc <dangcongduc38@gmail.com>
2025-06-19 17:15:24 +0700

    feat: complete logic pagination

--- ShopeeClone/src/pages/ProductList/ProductList.tsx
+++ ShopeeClone/src/pages/ProductList/ProductList.tsx
@@ -2,37 +2,69 @@ import useQueryParams from 'src/hooks/useQueryParams'
 import AsideFilter from './AsideFilter'
 import Product from './Product/Product'
 import SortProductList from './SortProductList'
-import { useQuery } from '@tanstack/react-query'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import productApi from 'src/apis/product.api'
+import Pagination from 'src/Components/Pagination'
+import { useState } from 'react'
+import type { ProductList, ProductListConfig } from 'src/types/product.type'
+import { isUndefined, omitBy } from 'lodash'
+
+export type QueryconfigType = {
+  [key in keyof ProductListConfig]: string
+}
 
 export default function ProductList() {
-  const queryParams = useQueryParams()
+  const queryParams: QueryconfigType = useQueryParams()
+  const queryConfig: QueryconfigType = omitBy(
+    {
+      page: queryParams.page || '1',
+      limit: queryParams.limit,
+      sort_by: queryParams.sort_by,
+      exclude: queryParams.exclude,
+      name: queryParams.name,
+      order: queryParams.order,
+      price_max: queryParams.price_max,
+      price_min: queryParams.price_min,
+      rating_filter: queryParams.rating_filter
+    },
+    isUndefined
+  )
   const { data } = useQuery({
-    queryKey: ['products', queryParams],
+    queryKey: ['products', queryConfig],
     queryFn: () => {
-      return productApi.getProducts(queryParams)
-    }
+      return productApi.getProducts(queryConfig as ProductListConfig)
+    },
+    placeholderData: keepPreviousData
   })
+
+  console.log('queryConfig: ', queryConfig)
+  console.log('data: ', data)
 
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
-        <div className='grid grid-cols-12 gap-6'>
-          <div className='col-span-3'>
-            <AsideFilter />
-          </div>
-          <div className='col-span-9 '>
-            <SortProductList />
-            <div className='mt-6 mx-3 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
-              {data &&
-                data.data.data.products.map((product) => (
+        {data && (
+          <div className='grid grid-cols-12 gap-6'>
+            <div className='col-span-3'>
+              {/* AsideFilter */}
+              <AsideFilter />
+            </div>
+            <div className='col-span-9 mx-3'>
+              {/* sort */}
+              <SortProductList />
+              {/* product */}
+              <div className='mt-6  grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
+                {data.data.data.products.map((product) => (
                   <div className='col-span-1' key={product._id}>
                     <Product product={product} />
                   </div>
                 ))}
+              </div>
+              {/* pagination */}
+              <Pagination queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
---
+++ ShopeeClone/thuattoan-doc.md
@@ -0,0 +1,43 @@
+# Tổng hợp thuật toán sử dụng trong website
+
+## hiển thị Pagination
+
+**Giả sử `range = 2`** được áp dụng cho 20 page:
+
+- Các trang đầu (`1, 2`)
+- Các trang cuối (`19, 20`)
+- Và các trang xung quanh `current_page`
+
+### 📍 Các trường hợp cụ thể
+
+---
+
+```text
+<!-- trường hợp 1 -->
+[1] 2 3 ... 19 20
+1 [2] 3 4 ... 19 20
+1 2 [3] 4 5 ... 19 20
+1 2 3 [4] 5 6 ... 19 20
+1 2 3 4 [5] 6 7 ... 19 20
+
+<!-- trường hợp 2 -->
+1 2 ... 4 5 [6] 8 9 ... 19 20
+
+1 2 ...13 14 [15] 16 17 ... 19 20
+
+<!-- trường hợp 3 -->
+1 2 ... 14 15 [16] 17 18 19 20
+1 2 ... 15 16 [17] 18 19 20
+1 2 ... 16 17 [18] 19 20
+1 2 ... 17 18 [19] 20
+1 2 ... 18 19 [20]
+```
+
+---
+
+**Ghi chú:**
+
+- `[]` thể hiện trang đang được chọn (`currentPage`)
+- Dấu `...` thể hiện phần đã được rút gọn để tránh hiển thị quá nhiều nút
+
+---

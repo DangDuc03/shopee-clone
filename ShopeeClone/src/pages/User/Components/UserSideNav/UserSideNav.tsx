e3200cf192f8b0cab89b800da703400b298d4514
Dang Cong Duc <dangcongduc38@gmail.com>
2025-08-09 23:17:21 +0700

    feat: done ui sidenav

--- ShopeeClone/src/pages/User/Components/UserSideNav/UserSideNav.tsx
+++ ShopeeClone/src/pages/User/Components/UserSideNav/UserSideNav.tsx
@@ -1,3 +1,103 @@
+import { Link } from 'react-router-dom'
+import path from 'src/constants/path'
+
 export default function UserSideNav() {
-  return <div>UserSideNav</div>
+  return (
+    <>
+      <div className='flex items-center border-b border-b-gray-200 py-4'>
+        <Link
+          to={path.profile}
+          className='flex flex-shrink-0 items-center overflow-hidden rounded-full border border-gray-400 px-2 py-2 hover:bg-gray-200/50'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='h-7 w-7 object-cover'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z'
+            />
+          </svg>
+        </Link>
+        <div className='flex-grow pl-4'>
+          <div className='mb-1 truncate text-xs font-semibold text-black'>Dang Cong Duc</div>
+          <Link className='flex items-center capitalize text-gray-600' to={path.profile}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='mr-1 h-4 w-4'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10'
+              />
+            </svg>
+            sửa hồ sơ
+          </Link>
+        </div>
+      </div>
+      <div className='mt-6'>
+        <Link to={path.profile} className='mb-3 flex items-center capitalize transition-colors'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='mr-2 h-5 w-5'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z'
+            />
+          </svg>
+          Hồ sơ của tôi
+        </Link>
+        <Link to={path.changePassword} className='mb-3 flex items-center capitalize transition-colors'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='mr-2 h-5 w-5'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88'
+            />
+          </svg>
+          Đổi mật khẩu
+        </Link>
+        <Link to={path.historyPurchase} className='mb-3 flex items-center capitalize transition-colors'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='mr-2 h-5 w-5'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='m9 14.25 6-6m4.5-3.493V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185ZM9.75 9h.008v.008H9.75V9Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm4.125 4.5h.008v.008h-.008V13.5Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z'
+            />
+          </svg>
+          Lịch sử mua hàng
+        </Link>
+      </div>
+    </>
+  )
 }
